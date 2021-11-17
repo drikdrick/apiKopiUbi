b2271d221621326e4e3be892c14a09dea008ec21
Fedrick Sulaiman Siagian <superiorsiagian@gmail.com>
2021-11-17 13:59:57 +0700

    login and register api

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const sql = require('../configs/database');
 
 module.exports = {
@@ -13,5 +14,52 @@ module.exports = {
       });
     });
   },
-
+  checkLogin(req, res) {
+    const data = {email: req.body.email, password: req.body.password};
+    // eslint-disable-next-line max-len
+    const query = 'Select * from users where email = ? and password = ? and role = 2';
+    sql.query(query, [data.email, data.password], (err, results)=>{
+      if (err) throw err;
+      if (results.length>0) {
+        res.json({
+          status: true,
+          message: 'Login berhasil!',
+          data: results[0],
+        });
+      } else {
+        res.json({
+          status: false,
+          message: 'Email/password salah. Mohon coba kembali.',
+        });
+      }
+    });
+  },
+  addUser(req, res) {
+    const data = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      name: req.body.name,
+      role: 2,
+      created_at: moment().format('YYYY-MM-DD HH:MM:SS'),
+      updated_at: moment().format('YYYY-MM-DD HH:MM:SS'),
+    };
+    const query = 'INSERT INTO users SET ?';
+    sql.query(query, data, (err, results)=>{
+      if (err) {
+        console.log(err);
+        res.json({
+          status: false,
+          message: 'Registrasi gagal, coba lagi.',
+          data: err,
+        });
+      } else {
+        res.json({
+          status: true,
+          message: 'Registrasi berhsil! Silahkan login.',
+          data: results,
+        });
+      };
+    });
+  },
 };
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,5 +2,7 @@ const router = require('express')();
 const {users} = require('../controllers');
 
 router.get('/users', users.getAllUser );
+router.post('/login', users.checkLogin);
+router.post('/register', users.addUser);
 
 module.exports = router;
